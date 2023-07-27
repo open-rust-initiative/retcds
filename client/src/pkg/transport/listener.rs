245f0126c950ad4d65ea1a5f74f519e76366f3d6
fangzhengchen123 <1849363589@qq.com>
2023-07-27 18:11:32 +0800

    add tlsinfo
    Signed-off-by: fangzhengchen123 <1849363589@qq.com>

--- client/src/pkg/transport/listener.rs
+++ client/src/pkg/transport/listener.rs
@@ -1,17 +1,23 @@
 use std::ffi;
-use std::fs::canonicalize;
-use std::io::{Error, ErrorKind};
+use std::fs::{canonicalize, File, Permissions, set_permissions};
+use std::io::{Error, ErrorKind, Write};
 use std::ops::Shl;
+use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
+use nix::libc::option;
 use num_bigint::{BigUint, ToBigUint};
-use openssl::asn1::{Asn1Integer, Asn1IntegerRef};
+use openssl::asn1::{Asn1Integer, Asn1IntegerRef, Asn1Time};
 use openssl::bn::BigNum;
+use openssl::ec::{EcGroup, EcKey};
+use openssl::hash::MessageDigest;
 use openssl::nid::Nid;
-use openssl::x509::{X509Builder, X509NameBuilder};
-use openssl_sys::{BIGNUM, X509};
+use openssl::pkey::PKey;
+use openssl::x509::{X509Builder, X509Extension, X509NameBuilder};
+use openssl::x509::extension::{ExtendedKeyUsage, KeyUsage, SubjectAlternativeName};
+use openssl_sys::{BIGNUM, BIO, PEM_write_bio_X509, X509};
 use rand::{Rng, thread_rng};
 use rustls::{Certificate, Connection};
-use slog::{Logger, warn};
+use slog::{info, Logger, warn};
 use crate::pkg::fileutil::fileutil::torch_dir_all;
 use crate::pkg::tlsutil::default_logger;
 
@@ -91,12 +97,12 @@ impl TLSInfo{
         }
     }
     pub fn string(&self) -> String{
-        format!("cert_file: {}, key_file: {}, client_cert_file: {}, client_key_file: {}, trusted_ca_file: {}, client_cert_auth: {}, CRL_file: {}, insecure_skip_verify: {}, skip_clientSAN_verify: {}, server_name: {}, allowed_cn: {}, allowed_hostname: {}, empty_cn: {}", self.cert_file, self.key_file, self.client_cert_file, self.client_key_file, self.trusted_ca_file, self.client_cert_auth, self.CRL_file, self.insecure_skip_verify, self.skip_clientSAN_verify, self.server_name, self.allowed_cn, self.allowed_hostname, self.empty_cn)
+       return  format!("cert_file: {}, key_file: {}, client_cert_file: {}, client_key_file: {}, trusted_ca_file: {}, client_cert_auth: {}, CRL_file: {}, insecure_skip_verify: {}, skip_clientSAN_verify: {}, server_name: {}, allowed_cn: {}, allowed_hostname: {}, empty_cn: {}, self_check: {}", self.cert_file, self.key_file, self.client_cert_file, self.client_key_file, self.trusted_ca_file, self.client_cert_auth, self.CRL_file, self.insecure_skip_verify, self.skip_clientSAN_verify, self.server_name, self.allowed_cn, self.allowed_hostname, self.empty_cn,self.self_cert);
     }
     pub fn empty(&self) -> bool{
         return self.cert_file == "" && self.key_file == ""
     }
-    pub fn self_cert(&mut self, dirpath:&str, hosts:Vec<String>, self_signed_cert_validity:usize, additional_usages:Vec<openssl::x509::extension::ExtendedKeyUsage>) -> Result<TLSInfo,Error>{
+    pub fn self_cert(&mut self, dirpath:&str, hosts:Vec<&str>, self_signed_cert_validity:usize, additional_usages:Option<&mut ExtendedKeyUsage>) -> Result<TLSInfo,Error>{
         self.logger = default_logger();
         if self_signed_cert_validity == 0{
             warn!(self.logger, "selfSignedCertValidity is invalid,it should be greater than 0,cannot generate cert");
@@ -104,9 +110,10 @@ impl TLSInfo{
         };
         torch_dir_all(dirpath).expect("torch dir error");
         let mut cert_file = PathBuf::new().join(dirpath).join("cert.pem");
-        let abs_cert_file = canonicalize(cert_file.clone()).expect("canonicalize error");
+        let mut abs_cert_file = cert_file.clone();
         let mut key_file = PathBuf::new().join(dirpath).join("key.pem");
-        let abs_key_file = canonicalize(key_file.clone()).expect("canonicalize error");
+        let mut abs_key_file = key_file.clone();
+        // abs_key_file.push("key.pem");
 
         if abs_key_file.exists() && abs_cert_file.exists(){
             self.cert_file = abs_cert_file.clone().to_str().unwrap().to_string();
@@ -129,22 +136,78 @@ impl TLSInfo{
 
         let mut name = X509NameBuilder::new().unwrap();
         name.append_entry_by_nid(Nid::ORGANIZATIONNAME,"etcd").unwrap();
+        let key_usage = KeyUsage::new().key_encipherment().digital_signature().build().unwrap();
+        let ext_key_usage = ExtendedKeyUsage::new().server_auth().build().unwrap();
         let num = BigNum::from_slice(serial_number.to_bytes_le().as_slice()).unwrap();
         let mut build = X509Builder::new().unwrap();
         build.set_serial_number(Asn1Integer::from_bn(&num).unwrap().as_ref()).unwrap();
         build.set_subject_name(name.build().as_ref()).unwrap();
-        return Ok(self.clone());
+        build.set_not_before(&Asn1Time::days_from_now(0).unwrap()).unwrap();
+        let after =&Asn1Time::days_from_now(self_signed_cert_validity as u32).unwrap();
+        build.set_not_after(&after.clone()).unwrap();
+        build.append_extension(key_usage).unwrap();
+        if additional_usages.is_some(){
+            build.append_extension(additional_usages.unwrap().server_auth().build().unwrap()).unwrap()
+        }
+        else {
+            build.append_extension(ext_key_usage).unwrap();
+        };
+        info!(default_logger(),"automatically generate certificates,certificate-validity-bound-not-after =>{}",after.clone().to_string());
+
+        let mut ext_builder = SubjectAlternativeName::new();
+        for host in hosts.clone() {
+            if let Ok(ip) = host.parse::<std::net::IpAddr>() {
+                ext_builder.ip(&ip.to_string());
+            } else {
+                ext_builder.dns(host);
+            }
+        }
+        let ext = ext_builder.build(&build.x509v3_context(None, None)).unwrap();
+        build.append_extension(ext).unwrap();
+
+        let group = EcGroup::from_curve_name(Nid::SECP521R1).unwrap();
+        let ecdsa = EcKey::generate(&group);
+        if ecdsa.clone().is_err(){
+            warn!(default_logger(),"cannot generate ecdsa key, reason =>{}",ecdsa.clone().err().unwrap());
+            return Err(Error::new(ErrorKind::Other, "cannot generate ecdsa key"));
+        }
+
+        let priv_key = PKey::from_ec_key(ecdsa.clone().unwrap()).unwrap();
+        build.set_pubkey(priv_key.clone().as_ref()).unwrap();
+        build.sign(priv_key.clone().as_ref(), MessageDigest::sha256()).unwrap();
+        let mut cert = build.build();
+        let pem = cert.to_pem();
+        if pem.is_err(){
+            warn!(default_logger(),"cannot generate pem, reason =>{}",pem.clone().err().unwrap());
+            return Err(Error::new(ErrorKind::Other, "cannot generate pem"));
+        }
+        let cert_out = File::create(cert_file.clone()).unwrap()
+            .write_all(cert.to_pem().unwrap().as_slice());
 
 
+        if cert_out.is_err() {
+            warn!(default_logger(),"cannot write cert file, reason =>{}",cert_out.err().unwrap().to_string().clone());
+            return Err(Error::new(ErrorKind::Other, "cannot write cert file"));
+        }
 
+        let key_out = File::create(key_file.clone()).unwrap()
+            .write_all(&ecdsa.unwrap().private_key_to_pem().unwrap());
+        if key_out.is_err() {
+            warn!(default_logger(),"cannot write key file, reason =>{}",key_out.err().unwrap().to_string().clone());
+            return Err(Error::new(ErrorKind::Other, "cannot write key file"));
+        }
+        set_permissions(key_file.clone(),Permissions::from_mode(0o600)).unwrap();
+
+        return self.self_cert(dirpath.clone(), hosts.clone(), self_signed_cert_validity.clone(), None);
 
     }
 }
 
 #[cfg(test)]
 mod tests{
-    use openssl::bn::BigNumContext;
-    use openssl::rand::rand_bytes;
+    use std::io::BufReader;
+    use openssl::x509::extension::{ExtendedKeyUsage, KeyUsage};
+    use openssl::x509::X509;
     use super::*;
 
     #[test]
@@ -154,7 +217,30 @@ mod tests{
         let serial_number: BigUint = BigUint::from_bytes_le(&rng.gen::<[u8; 16]>()) % &serial_number_limit;
         let num = BigNum::from_slice(serial_number.to_bytes_le().as_slice()).unwrap();
         let x = Asn1Integer::from_bn(&num).unwrap();
+        let key_usage = KeyUsage::new().key_encipherment().digital_signature().build().unwrap();
+
 
         println!("{}", x.to_bn().unwrap().to_string());
+    }
+
+    #[test]
+    fn test_self_cert(){
+        let mut tls_info = TLSInfo::new();
+        let hosts = vec!["127.0.0.1:0"];
+        let dirpath = "/tmp/test_self_cert";
+        let self_signed_cert_validity = 365;
+        let mut binding = ExtendedKeyUsage::new();
+        let additional_usages = binding.client_auth();
+        let tls_info = tls_info.self_cert(dirpath, hosts, self_signed_cert_validity, Some(additional_usages));
+        println!("{:?}", tls_info.unwrap().string());
+    }
+
+    #[test]
+    fn test_self_cert_res(){
+        let cert_file = File::open("/tmp/test_self_cert/cert.pem").unwrap();
+        let cert_reader = BufReader::new(cert_file);
+        let cert = X509::from_pem(cert_reader.buffer()).unwrap();
+        let text = cert.to_text().unwrap();
+        println!("{:?}", text);
     }
 }
